ab70321193638671bab03e97d7a66d6a71680606
RoyTang13 <136442538+RoyTang13@users.noreply.github.com>
2025-11-24 18:20:12 +0800

    JavaScript Update for registration, login

--- Assignment/js/app.js
+++ Assignment/js/app.js
@@ -10,11 +10,64 @@
 
 $(() => {
 
+    // Autofocus
+    $('form :input:not(button):first').focus();
+    $('.err:first').prev().focus();
+    $('.err:first').prev().find(':input:first').focus();
+
     // Initiate GET request
     $('[data-get]').on('click', e => {
         e.preventDefault();
         const url = e.target.dataset.get;
         location = url || location;
     });
+
+    // Initiate POST request
+    $('[data-post]').on('click', e => {
+        e.preventDefault();
+        const url = e.target.dataset.post;
+        const f = $('<form>').appendTo(document.body)[0];
+        f.method = 'POST';
+        f.action = url || location;
+        f.submit();
+    });
+
+    // Reset form
+    $('[type=reset]').on('click', e => {
+        e.preventDefault();
+        location = location;
+    });
+
+    // Auto uppercase
+    $('[data-upper]').on('input', e => {
+        const a = e.target.selectionStart;
+        const b = e.target.selectionEnd;
+        e.target.value = e.target.value.toUpperCase();
+        e.target.setSelectionRange(a, b);
+    });
+
+    // Confirmation message
+    $('[data-confirm]').on('click', e => {
+        const text = e.target.dataset.confirm || 'Are you sure?';
+        if (!confirm(text)) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+        }
+    });
+
+    $(document).ready(function () {
+        // Toggle dropdown on button click
+        $('.user-btn').click(function (e) {
+            e.stopPropagation(); // Prevent closing immediately
+            $(this).siblings('.dropdown-content').toggleClass('show');
+        });
+
+        // Close dropdown if clicked outside
+        $(document).click(function () {
+            $('.dropdown-content').removeClass('show');
+        });
+    });
+
+
 
 });
