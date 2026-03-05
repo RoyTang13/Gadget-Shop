f2eff9aa45b3c7d177bb51ba8ec54a9c15e2d39b
RoyTang13 <136442538+RoyTang13@users.noreply.github.com>
2026-03-05 11:58:48 +0800

    test1234

--- Assignment/index.php
+++ Assignment/index.php
@@ -14,7 +14,7 @@ include '_head.php';
 
 <div class="content-area">
     <div class="wrapper">
-        <h2>What is TechNest?</h2>
+        <h2>What is TechNest?s</h2>
         <p>TechNest is all about bringing your music to life. We design and create earphones that combine cutting-edge technology with sleek, modern style. From classic wired earphones to the latest wireless models, our products deliver crystal-clear sound, deep bass, and all-day comfort. At TechNest, we believe everyone deserves a premium audio experience, whether you’re working, gaming, exercising, or just relaxing with your favorite tracks. Our team is passionate about innovation, quality, and style, making sure every pair of earphones not only sounds amazing but looks great too. Join the TechNest community and experience the perfect mix of performance, design, and freedom.</p>
     </div>
 </div>
